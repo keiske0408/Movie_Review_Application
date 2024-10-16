bf99a636974510fbbbb99f8735207f4a3474a057
keiske0408 <kieske0408@gmail.com>
2024-10-16 19:55:46 +0800

    associate user to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_16_112710) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_16_114419) do
   create_table "movies", charset: "utf8mb4", force: :cascade do |t|
     t.string "Title"
     t.text "Blurb"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_16_112710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
