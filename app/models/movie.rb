534d69350624b7df88e7b1fe7c8d6a36f696055e
keiske0408 <kieske0408@gmail.com>
2024-10-21 20:22:22 +0800

    add rating logic and modify view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,9 @@ class Movie < ApplicationRecord
   has_many :movie_genre_ships
   has_many :genres, through: :movie_genre_ships
   has_many :reviews
+
+  def average_rating
+    return 0 if reviews.empty? # Avoid division by zero
+    reviews.average(:rating).round(2) # Round to 2 decimal places
+  end
 end
