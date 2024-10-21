b6db58ae2fcaa06fcf1c0b85f32df3a86a736d23
keiske0408 <kieske0408@gmail.com>
2024-10-21 20:03:27 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'home/about'
   root 'home#index'
   devise_for :users
+  resources :movies do
+    resources :reviews, except: :show
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
